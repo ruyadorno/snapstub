659dafb0c64e996a1e4db8718c34de8d7961c770
Andres Valencia <andres.valencia@appdirect.com>
2017-04-27 16:50:46 -0400

    print routes when running start cmd

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const stubborn = require('stubborn-server');
 const out = require('simple-output');
+const globby = require('globby');
+const methods = require('methods');
 
 function startCmd(opts) {
 	const mockFolderName = opts.mockFolderName;
@@ -16,10 +20,35 @@ function startCmd(opts) {
 		});
 		out.success('Successfully launched snapstub server on: ' +
 			'http://localhost:' + port);
+		printRoutes(mockFolderName, port);
 	} catch (e) {
 		out.error('Failed to launch snapstub server');
 	}
 }
 
-module.exports = startCmd;
+function printRoutes(srcPath, port) {
+	globby('**/', { cwd: srcPath })
+		.then(paths => {
+			paths.forEach(pathName => {
+				if (methods.some(method => {
+					const fileLocation = path.join(srcPath, pathName, method);
+					return [
+						`${fileLocation}.js`,
+						`${fileLocation}.json`
+					].some(fileName => {
+						try {
+							fs.accessSync(fileName, fs.constants.R_OK);
+							return true;
+						} catch(err) {
+							return false;
+						}
+					});
+				})) {
+					out.info(`http://localhost:${port}/${pathName}`);
+				}
+			});
+		})
+		.catch(out.error);
+}
 
+module.exports = startCmd;
