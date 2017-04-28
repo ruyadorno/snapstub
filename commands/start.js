5c26fda9a4c049a061f513e35faa31715611a35a
Ruy Adorno <ruyadorno@hotmail.com>
2017-04-28 18:04:35 -0400

    Fixed lint issues

--- commands/start.js
+++ commands/start.js
@@ -25,9 +25,8 @@ function startCmd(opts) {
 		out.error('Failed to launch snapstub server');
 	}
 }
-
 function printRoutes(srcPath, port) {
-	globby('**/', { cwd: srcPath })
+	globby('**/', {cwd: srcPath})
 		.then(paths => {
 			paths.forEach(pathName => {
 				if (methods.some(method => {
@@ -39,7 +38,7 @@ function printRoutes(srcPath, port) {
 						try {
 							fs.accessSync(fileName, fs.constants.R_OK);
 							return true;
-						} catch(err) {
+						} catch (err) {
 							return false;
 						}
 					});
